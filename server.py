2cdab09e1bd6da6add6c58623f913b808b44ad83
edward wang! <53409587+wang-edward@users.noreply.github.com>
2021-10-16 23:42:05 -0400

    processed json from db

--- server.py
+++ server.py
@@ -3,43 +3,55 @@ from firebase import firebase
 from datetime import datetime 
 from datetime import date
 import time
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import json
 # import firebase_admin
 # from firebase_admin import credentials
 firebase = firebase.FirebaseApplication('https://occupancytracker-69a70-default-rtdb.firebaseio.com/', None)
 
-text = 'edad'
-
 
 number_of_people = 213
-cur_time = str(int(time.time()))
+cur_time = str(datetime.now().time().strftime("%H:%M:%S:%f"))
 cur_date = date.today().strftime("/%Y/%m/%d/")
 
 def update_time():
-    cur_time = str(int(time.time()))
+    # cur_time = str(int(time.time()))
+    cur_time = str(datetime.now().time().strftime("%H:%M:%S"))
     cur_date = date.today().strftime("/%Y/%m/%d/")
 
 #creates new folder for number of people at that time
 def update_db(num_people):
     update_time()
 
-    index = cur_date + cur_time
-
-    print(index)
+    index = cur_date + "/" + cur_time
 
     firebase.post(index, num_people)
+
+
 
 def graph():
     update_time()
     results = firebase.get(cur_date, None)
-    print(results)
-    for value in results:
-        print((results[value])[0])
 
+    answer_num = [] #output list
+    answer_time = []
 
+    for temp in results:
+        for key, value in results[temp].items():
+            answer_num.append(value)
+            answer_time.append(temp)
+
+    for x in answer_time:
+        print(x)
+    for i in answer_num:
+        print(i)
+
+    # fig, ax = plt.subplots()
+    # ax.plot(answer_time,answer_num)
+    
+
+# testing!!! -------------------------------------------------
 
 # update_db(number_of_people)
-graph()
-    
-# print(cur_date)
+# graph()
