14747478e75a85db96f80434d9efe55388219390
edward wang! <53409587+wang-edward@users.noreply.github.com>
2021-10-17 09:40:13 -0400

    fixed graph

--- server.py
+++ server.py
@@ -2,22 +2,24 @@ from firebase import firebase
 from datetime import datetime 
 from datetime import date
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
 
 firebase = firebase.FirebaseApplication('https://occupancytracker-69a70-default-rtdb.firebaseio.com/', None)
 
-cur_time = str(datetime.now().time().strftime("%H:%M:%S:%f"))
+cur_time = str(datetime.now().time().strftime("%H:%M:%S-%f"))
 cur_date = date.today().strftime("/%Y/%m/%d/")
 
 #creates new folder for number of people at that time
 def update_db(num_people):
-    cur_time = str(datetime.now().time().strftime("%H:%M:%S:%f"))
+    cur_time = str(datetime.now().time().strftime("%H:%M:%S-%f"))
     cur_date = date.today().strftime("/%Y/%m/%d/")
-    index = cur_date + "/" + cur_time
+    index = cur_date + cur_time
 
     firebase.post(index, num_people)
 
 def graph():
-    cur_time = str(datetime.now().time().strftime("%H:%M:%S:%f"))
+    cur_time = str(datetime.now().time().strftime("%H:%M:%S-%f"))
     cur_date = date.today().strftime("/%Y/%m/%d/")
 
     results = firebase.get(cur_date, None)
@@ -28,9 +30,14 @@ def graph():
     for temp in results:
         for key, value in results[temp].items():
             answer_num.append(value)
-            answer_time.append(temp)
+            answer_time.append(temp.replace("-","."))
+
+    dates = pd.to_datetime(answer_time)
 
     fig, ax = plt.subplots()
-    ax.plot(answer_time,answer_num)
+    ax.plot(dates,answer_num)
+    ax.xaxis_date()
+    fig.autofmt_xdate()
     plt.show()
     
+graph()
